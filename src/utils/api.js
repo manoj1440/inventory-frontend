c709501011818f3ebcb78ecc1de0fad83679ed3c
manoj1440 <malviya.manoj1997@gmail.com>
2023-08-26 14:35:28 +0530

    Added .env BASE_URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import { message } from 'antd';
 import axios from 'axios';
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:4000';
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
